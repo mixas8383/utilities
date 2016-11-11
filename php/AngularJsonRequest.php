bb452ba0f77ad0b600d762a2b08dfbbaedb58746
mixas <mixas8383@yahoo.com>
2016-11-11 10:59:02 +0400

    php library

--- php/AngularJsonRequest.php
+++
@@ -1,4 +0,0 @@
-if(isset($_SERVER["CONTENT_TYPE"]) && strpos($_SERVER["CONTENT_TYPE"], "application/json") !== false) {
-    $_POST = array_merge($_POST, (array) json_decode(trim(file_get_contents('php://input')), true));
-    $_REQUEST = array_merge($_REQUEST, (array) json_decode(trim(file_get_contents('php://input')), true));
-}
